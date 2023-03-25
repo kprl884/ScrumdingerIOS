370b8b42be21a843f463a4d33daf106cf6febc1e
kprl84 <kprl884@gmail.com>
2023-03-25 13:04:49 +0300

    Initialized the project and add Horizontal and Vertical labels and put text into the labels

---
+++ Scurumdinger/Scurumdinger/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scurumdinger
+//
+//  Created by Apple on 25.03.2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scurumdinger/Scurumdinger/ScurumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScurumdingerApp.swift
+//  Scurumdinger
+//
+//  Created by Apple on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScurumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
